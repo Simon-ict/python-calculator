f5b88a3997d70aae9e7c3a5e5c619945133e31d3
Simon-ict <109569199+Simon-ict@users.noreply.github.com>
2024-11-08 19:37:20 +0000

    done fixed bugs and create tester

--- calculator.py
+++ calculator.py
@@ -3,23 +3,38 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if(a < 0 and b < 0):
+            a = 0 - a 
+            b = 0 - b 
+        if(b >= 0):
+            for i in range(b):
+                result = self.add(result, a)
+        elif(a >= 0):
+            for i in range(a):
+                result = self.add(result, b)
+        
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        check = 0
+        if(a < 0 or b < 0):
+            a = 0 - a
+            b = 0 - b
+            check = 1
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        if(check == 1):
+            return -result
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
